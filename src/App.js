0536c4eebb939c10433bf2b22dd405812bc6d7ca
Samuel Garcia <samfrontdev@gmail.com>
2020-11-05 17:22:35 -0500

    About Area created

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Work from './components/Work'
+import About from './components/About'
 import Rayo from './assets/images/rayo.svg'
 import Sticker from './assets/images/sticker.svg'
+import Planet from './assets/images/planet.svg'
 import './assets/styles/App.scss';
 
 function App() {
@@ -11,10 +13,12 @@ function App() {
     <>
       <img className="light" src={Rayo} alt="Light"/>
       <img className="sticker" src={Sticker} alt="Holo"/>
+      <img className="planet" src={Planet} alt="Pluto"/>
       <div className="App">
         <Header/>
         <Hero/>
         <Work/>
+        <About/>
       </div>
     </>
   );
---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../assets/styles/components/About.css'
+
+export default function About() {
+  return(
+    <div className="About">
+      <div className="About_container">
+        <div className="About_content">
+          <h1>ABOUT ME</h1>
+          <div className="about_text">
+            <p>Hello i’m Samuel García a 16 years old
+            frontend developer from Colombia.</p>
+            <p>My journey in tech starts when i was
+            13 and watch “The Social Network” movie, since that moment i 
+            decided to create tech and not only consume.</p>
+            <p>I like retro music, skateboards, space exploration, indie movies and CODE</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
