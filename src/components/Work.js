29c103f8bb8f7b2faa5779e16b86494f13546c26
Samuel Garcia <samfrontdev@gmail.com>
2020-11-10 18:47:01 -0500

    Background updated

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,11 +8,11 @@ export default function Work() {
       <div className="Work_container">
         <div className="Work_content">
           <h1>WORK</h1>
-          <Card 
-            CardTitle="UziDay - CEO"
-            CardText="This was my first startup release, at the age of 14 (Now 16), we were creating a productivity application with gamification"
-            CardLink="https://www.dinero.com/edicion-impresa/management/articulo/historia-de-samuel-garcia-cofundador-de-uziday/266407"
-          />
+            <Card 
+              CardTitle="UziDay - CEO"
+              CardText="This was my first startup release, at the age of 14 (Now 16), we were creating a productivity application with gamification"
+              CardLink="https://www.dinero.com/edicion-impresa/management/articulo/historia-de-samuel-garcia-cofundador-de-uziday/266407"
+            />
           <Card 
             CardTitle="Meniacs - Founder"
             CardText="I created this clothing store to reflect all that tech culture means to me and many more people"
