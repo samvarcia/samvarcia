3052dbd1827520374a76c9d7ea70d7c341ddc89e
Samuel Garcia <samfrontdev@gmail.com>
2020-11-12 06:49:50 -0500

    Corrected card component structure

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,15 +2,19 @@ import React from 'react'
 import '../assets/styles/components/Card.css'
 
 const Card = ({ CardTitle, CardText, CardLink }) => (
-  <div className="card">
-    <div className="card_container">
-      <div className="card_content">
-        <h2 className="justify">{CardTitle}</h2>
-        <p className="justify">{CardText}</p>
-        <a target="blank" className="center" href={CardLink}>VIEW</a>
+  <>
+    <a className="cardlink" href={CardLink}>
+      <div className="card">
+        <div className="card_container">
+          <div className="card_content"> 
+            <h2 className="justify">{CardTitle}</h2>
+            <p className="justify">{CardText}</p>
+            <a target="blank" className="center" href={CardLink}>VIEW</a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    </a>
+  </>
 )
 
 export default Card;
