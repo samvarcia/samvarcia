f21458601163f17ebcd3632fae9d8c08299f3dac
Samuel Garcia <samfrontdev@gmail.com>
2023-04-04 10:29:12 -0500

    framer

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,27 @@
+"use client";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
+import { motion } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Image
-        src="/SAMVASIGN.svg"
-        alt="samvarcia"
-        width={400}
-        height={100}
-        priority
-      />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+      >
+        <Image
+          src="/SAMVASIGN.svg"
+          alt="samvarcia"
+          width={400}
+          height={100}
+          priority
+        />
+      </motion.div>
     </main>
   );
 }
