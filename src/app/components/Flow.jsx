b18b2ab697f531f4b9df2590adcd08cc1cb83576
Samuel Garcia <samfrontdev@gmail.com>
2023-04-07 19:11:30 -0500

    smooth testing

--- src/app/components/Flow.jsx
+++ src/app/components/Flow.jsx
@@ -1,14 +1,21 @@
 import styles from "./Flow.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 export default function Flow() {
+  const { scrollXProgress } = useScroll();
+  const scaleX = useSpring(scrollXProgress, {
+    stiffness: 500,
+    damping: 70,
+    restDelta: 0.001,
+  });
   return (
     <div className={styles.flowwrap}>
-      <div className={styles.flow}>
+      <motion.div className={styles.flow} style={scaleX}>
         <Image
-          className={styles.bloop}
-          src="/bloop.gif"
+          className={styles.web}
+          src="/bloop.svg"
           width={773}
           height={527}
         />
@@ -18,7 +25,19 @@ export default function Flow() {
           width={287}
           height={511}
         />
-      </div>
+        <Image
+          className={styles.web}
+          src="/saikoro.svg"
+          width={773}
+          height={527}
+        />
+        <Image
+          className={styles.vertical}
+          src="/icarus.svg"
+          width={287}
+          height={511}
+        />
+      </motion.div>
     </div>
   );
 }
