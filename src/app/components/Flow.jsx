2af9b7291a60cb18fa895441a44ea90175adeb57
Samuel Garcia <samfrontdev@gmail.com>
2023-04-25 15:17:59 -0500

    basic carousel

--- src/app/components/Flow.jsx
+++ src/app/components/Flow.jsx
@@ -1,144 +1,80 @@
+import { useState, useEffect } from "react";
 import styles from "./Flow.module.css";
-import Image from "next/image";
-import React, { useState, useEffect, useRef } from "react";
-
-import { motion, useScroll, useSpring } from "framer-motion";
-import Link from "next/link";
+const images = [
+  "/bloop.png",
+  "/saikoro.png",
+  "/BN.png",
+  "/AEXUI.png",
+  "/lightlamp.gif",
+  "/icarus.svg",
+  "/thinkmore.gif",
+];
 
 export default function Flow() {
-  const containerRef = useRef(null);
-
-  useEffect(() => {
-    const containerHeight = containerRef.current.offsetHeight;
-    const scrollHeight = containerRef.current.scrollHeight;
-
-    let currentScrollTop = 0;
-
-    const intervalId = setInterval(() => {
-      currentScrollTop += 1;
-
-      if (currentScrollTop > scrollHeight - containerHeight) {
-        currentScrollTop = 0;
-      }
-
-      containerRef.current.scrollTop = currentScrollTop;
-    }, 10);
-
-    return () => clearInterval(intervalId);
-  }, []);
-  const imageUrls = [
-    "/bloop2.png",
-    "/AEXUI.png",
-    "/saikoro2.png",
-    "/icarus.svg",
-    "/lightlamp.gif",
-    "/INSTW.png",
-    "/BN.png",
-    "/thinkmore.gif",
-    // Add more images as needed
-  ];
-  // const flowRef = useRef();
-  // const constraintsRef = useRef(null);
-  // const { scrollXProgress } = useScroll();
-  // const scaleX = useSpring(scrollXProgress, {
-  //   stiffness: 100,
-  //   damping: 30,
-  //   restDelta: 0.001,
-  // });
-  // const { scrollYProgress } = useScroll();
-  // const scaleY = useSpring(scrollYProgress, {
-  //   stiffness: 100,
-  //   damping: 30,
-  //   restDelta: 0.001,
-  // });
-
-  // const handleWheel = (evt) => {
-  //   const flowbar = flowRef.current;
-  //   if (flowbar) {
-  //     flowbar.scrollLeft += evt.deltaY;
-  //   }
-  //   // console.log("WHEEL");
-  // };
-
   return (
-    <div
-      ref={containerRef}
-      className={styles.imageList}
-      // style={{
-      //   transform: `translateY(-${scrollTop}px)`,
-      // }}
-    >
-      {imageUrls.map((imageUrl, index) => (
-        <div key={index} className={styles.imageContainer}>
-          <img src={imageUrl} alt={`Image ${index + 1}`} />
-        </div>
-      ))}
-    </div>
-    // <div className={styles.flowwrap}>
-    //   <motion.div className={styles.flow}>
-    //     <div className={styles.space}></div>
-    //     <Link href={"/bloop"}>
-    //       <Image
-    //         className={styles.web}
-    //         src="/bloop.png"
-    //         width={773}
-    //         height={527}
-    //         priority
-    //       />
-    //     </Link>
-    //     <Image
-    //       className={styles.vertical}
-    //       src="/AEXUI.png"
-    //       width={287}
-    //       height={511}
-    //       priority
-    //     />
-    //     <Image
-    //       className={styles.web}
-    //       src="/saikoro.png"
-    //       width={773}
-    //       height={527}
-    //       priority
-    //     />
-    //     <Image
-    //       className={styles.vertical}
-    //       src="/icarus.svg"
-    //       width={287}
-    //       height={511}
-    //       priority
-    //     />
-    //     <Image
-    //       className={styles.web}
-    //       src="/lightlamp.gif"
-    //       width={773}
-    //       height={527}
-    //       priority
-    //     />
-    //     <Image
-    //       className={styles.poster}
-    //       src="/INSTW.png"
-    //       width={287}
-    //       height={511}
-    //       alt="THE ROLLING STONES INSPIRED POSTER - SAMVARCIA"
-    //       priority
-    //     />
-    //     <Image
-    //       src="/BN.png"
-    //       width={500}
-    //       height={500}
-    //       alt="BONNE NUIT"
-    //       className={styles.square}
-    //       priority
-    //     />
-    //     <Image
-    //       className={styles.video}
-    //       src="/thinkmore.gif"
-    //       width={773}
-    //       height={527}
-    //       alt="AELIZA INSPIRED VIDEO - SAMVARCIA"
-    //       priority
-    //     />
-    //   </motion.div>
+    // <div className={styles.flow}>
+    //   <div className={styles.flowInner}>
+    //     <img src="/bloop.png" />
+    //     <img src="/saikoro.png" />
+    //     <img src="/BN.png" />
+    //     <img src="/AEXUI.png" />
+    //     <img src="/lightlamp.gif" />
+    //     <img src="/icarus.svg" />
+    //     <img src="/thinkmore.gif" />
+    //   </div>
     // </div>
+    <div className={styles.flow}>
+      <div className={styles.ticker}>
+        <img
+          class="w-64 mx-4 self-start flex-none"
+          src="https://via.placeholder.com/256x341/f4ccccff/434343?text=1"
+        />
+        <img
+          class="w-64 mx-4 self-start flex-none"
+          src="https://via.placeholder.com/256x192/f4ccccff/434343?text=2"
+        />
+        <img
+          class="w-64 mx-4 self-start flex-none"
+          src="https://via.placeholder.com/256x341/f4ccccff/434343?text=3"
+        />
+        <img
+          class="w-64 mx-4 self-start flex-none"
+          src="https://via.placeholder.com/256x192/f4ccccff/434343?text=4"
+        />
+        <img
+          class="w-64 mx-4 self-start flex-none"
+          src="https://via.placeholder.com/256x341/f4ccccff/434343?text=5"
+        />
+        <img
+          class="w-64 mx-4 self-start flex-none"
+          src="https://via.placeholder.com/256x192/f4ccccff/434343?text=6"
+        />
+
+        <img
+          class="w-64 mx-4 self-start flex-none"
+          src="https://via.placeholder.com/256x341/f4ccccff/434343?text=1"
+        />
+        <img
+          class="w-64 mx-4 self-start flex-none"
+          src="https://via.placeholder.com/256x192/f4ccccff/434343?text=2"
+        />
+        <img
+          class="w-64 mx-4 self-start flex-none"
+          src="https://via.placeholder.com/256x341/f4ccccff/434343?text=3"
+        />
+        <img
+          class="w-64 mx-4 self-start flex-none"
+          src="https://via.placeholder.com/256x192/f4ccccff/434343?text=4"
+        />
+        <img
+          class="w-64 mx-4 self-start flex-none"
+          src="https://via.placeholder.com/256x341/f4ccccff/434343?text=5"
+        />
+        <img
+          class="w-64 mx-4 self-start flex-none"
+          src="https://via.placeholder.com/256x192/f4ccccff/434343?text=6"
+        />
+      </div>
+    </div>
   );
 }
