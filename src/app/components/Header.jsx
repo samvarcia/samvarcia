d0a049ef6eaf8cf59f1c73b229fe1dbaa6833b27
Samuel Garcia <samfrontdev@gmail.com>
2023-05-11 16:07:25 -0500

    Header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,37 +1,37 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { usePathname, useRouter } from "next/navigation";
-
+import styles from "./Header.module.css";
 const Header = ({ pages }) => {
   const router = useRouter();
   const [currentIndex, setCurrentIndex] = useState(0);
-  // const pathname = usePathname();
+  const pathname = usePathname();
 
   useEffect(() => {
-    const index = pages.findIndex((page) => page.path === router.pathname);
+    const index = pages.findIndex((page) => page.path === pathname);
     setCurrentIndex(index);
   }, [router.pathname]);
 
   const handlePrevious = () => {
-    if (currentIndex > 0) {
+    if (currentIndex === 0) {
+      router.push(pages[pages.length - 1].path);
+    } else {
       router.push(pages[currentIndex - 1].path);
     }
   };
 
   const handleNext = () => {
-    if (currentIndex < pages.length - 1) {
+    if (currentIndex === pages.length - 1) {
+      router.push(pages[0].path);
+    } else {
       router.push(pages[currentIndex + 1].path);
     }
   };
 
   return (
-    <div>
-      <button onClick={handlePrevious} disabled={currentIndex === 0}>
-        Previous
-      </button>
-      <button onClick={handleNext} disabled={currentIndex === pages.length - 1}>
-        Next
-      </button>
+    <div className={styles.Header}>
+      <p onClick={handlePrevious}>Previous</p>
+      <p onClick={handleNext}>Next</p>
     </div>
   );
 };
--- src/app/icarus/layout.js
+++ src/app/icarus/layout.js
@@ -1,12 +1,23 @@
 import styles from "./styles.module.css";
 import Menu from "../components/Menu";
-
+import Header from "../components/Header";
+const pages = [
+  { path: "/bloop" },
+  { path: "/bn" },
+  { path: "/saikoro" },
+  { path: "/aex" },
+  { path: "/glow" },
+  { path: "/instw" },
+  { path: "/icarus" },
+  { path: "/thinkmore" },
+];
 export default function DashboardLayout({
   children, // will be a page or nested layout
 }) {
   return (
     <div>
       <section className={styles.about}>
+        <Header pages={pages} />
         {children}
         <Menu />
       </section>
