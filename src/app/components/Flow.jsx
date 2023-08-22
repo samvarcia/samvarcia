b762ed7d756b82e4e52cc20b353e8cf652a15b2c
Samuel Garcia <samfrontdev@gmail.com>
2023-08-22 08:40:17 -0500

    Scrollbar hidden

--- src/app/components/Flow.jsx
+++ src/app/components/Flow.jsx
@@ -4,83 +4,62 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./Flow.module.css";
 import { motion, AnimatePresence } from "framer-motion";
-
-const images = [
+const projects = [
   {
-    src: "https://raw.githubusercontent.com/samvarcia/samvarcia/main/public/bloop.png",
+    src: "/BloopLanding.png",
     class: "web",
     route: "/bloop",
     alt: "Bloop - Samvarcia",
   },
   {
-    src: "https://raw.githubusercontent.com/samvarcia/samvarcia/main/public/BN.png",
+    src: "/BN.png",
     class: "bn",
     route: "/bn",
     alt: "BonneNuit - Samvarcia",
   },
   {
-    src: "https://raw.githubusercontent.com/samvarcia/samvarcia/main/public/saikoro.png",
+    src: "/saikoro.png",
     class: "web",
     route: "/saikoro",
     alt: "Sa!koro - Samvarcia",
   },
   {
-    src: "https://raw.githubusercontent.com/samvarcia/samvarcia/main/public/AEXWRAP.png",
+    src: "/AEXWRAP.png",
     class: "think",
     route: "/aex",
     alt: "AEX - Samvarcia",
   },
   {
-    src: "https://raw.githubusercontent.com/samvarcia/samvarcia/main/public/greenlamp.gif",
+    src: "/greenlamp.gif",
     class: "think",
     route: "/glow",
     alt: "GLOW - Samvarcia",
   },
   {
-    src: "https://raw.githubusercontent.com/samvarcia/samvarcia/main/public/INSTW.png",
+    src: "/INSTW.png",
     class: "poster",
     route: "/instw",
     alt: "TheRollingStones - Samvarcia",
   },
   {
-    src: "https://raw.githubusercontent.com/samvarcia/samvarcia/main/public/ICARUS.png",
+    src: "/ICARUS.png",
     class: "vertical",
     route: "/icarus",
     alt: "ICARUS - Samvarcia",
   },
   {
-    src: "https://raw.githubusercontent.com/samvarcia/samvarcia/main/public/thinkmore.gif",
+    src: "/thinkmore.gif",
     class: "think",
     route: "/thinkmore",
     alt: "AELIZA - Samvarcia",
   },
 ];
 const Flow = () => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((currentIndex) =>
-        currentIndex === images.length - 1 ? 0 : currentIndex + 1
-      );
-    }, 1500);
-    return () => clearInterval(interval);
-  }, []);
-
-  const currentImage = images[currentImageIndex];
   return (
-    <div className={styles.projects}>
-      <Link href={currentImage.route}>
-        <motion.img
-          key={currentImageIndex}
-          src={images[currentImageIndex].src}
-          alt={currentImage.alt}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.8 }}
-          className={styles[currentImage.class]}
-        />
-      </Link>
+    <div className={styles.projectsHorizon}>
+      {projects.map((project, index) => (
+        <Image src={project.src} width="400" height="450" quality={100} />
+      ))}
     </div>
   );
 };
